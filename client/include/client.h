3d0ea2ac8fb020b180f23681e28e2e4b0acf6871
Benoit Lormeau <benoit.lormeau@epitech.eu>
2018-04-25 14:59:45 +0200

    Added via git_push

--- client/include/client.h
+++ client/include/client.h
@@ -6,5 +6,43 @@
 */
 
 #pragma once
+#define _GNU_SOURCE
 
 ////////////////////////////////////////////////////////////////////////////////
+
+#include <limits.h>
+#include <pwd.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "cnet/tcp_listener.h"
+#include "cnet/tcp_socket.h"
+#include "cnet/ftp.h"
+#include "my/regex.h"
+#include "my/string.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+#define UNUSED __attribute__((unused))
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef struct client
+{
+	tcp_socket_t *control_socket;
+	ip_address_t control_channel_ip;
+	unsigned short control_channel_port;
+
+	tcp_socket_t *data_socket;
+	tcp_listener_t *data_listener;
+	ip_address_t data_channel_ip;
+	unsigned short data_channel_port;
+
+	bool passive;
+} client_t;
